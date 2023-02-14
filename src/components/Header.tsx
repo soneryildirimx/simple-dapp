524a2dde57e666320958c9a76e2715f261797b69
Soner Yıldırım <soneryildirimx@gmail.com>
2023-02-14 11:18:04 +0300

    send transaction complete

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,9 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit";
 const Header = () => {
     return (
-        <header className="p-10 flex justify-between items-center">
+        <header className="p-10 flex justify-between items-center md:flex-row flex-col md:gap-0 gap-4">
             <h3 className="text-lg">Simple Dapp</h3>
-            <ConnectButton chainStatus={"full"} showBalance />
+            <ConnectButton chainStatus={"full"} />
         </header>
     );
 };
--- src/components/SendTransaction.tsx
+++ src/components/SendTransaction.tsx
@@ -34,28 +34,42 @@ export function SendTransaction() {
                 e.preventDefault();
                 sendTransaction?.();
             }}
+            className="max-w-lg w-full"
         >
             <input
                 aria-label="Recipient"
                 onChange={(e) => setTo(e.target.value)}
                 placeholder="0xA0Cf…251e"
                 value={to}
+                type="text"
+                disabled={isLoading}
+                className="mb-4 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
             <input
                 aria-label="Amount (ether)"
                 onChange={(e) => setAmount(e.target.value)}
                 placeholder="0.05"
                 value={amount}
+                type="number"
+                disabled={isLoading}
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
             />
-            <button disabled={isLoading || !sendTransaction || !to || !amount}>
+            <button
+                className="mt-4  text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                disabled={isLoading || !sendTransaction || !to || !amount}
+            >
                 {isLoading ? "Sending..." : "Send"}
             </button>
             {isSuccess && (
-                <div>
+                <div className="text-center py-4">
                     Successfully sent {amount} ether to {to}
                     <div>
-                        <a href={`https://etherscan.io/tx/${data?.hash}`}>
-                            Etherscan
+                        <a
+                            target={"_blank"}
+                            className="underline underline-offset-4"
+                            href={`https://etherscan.io/tx/${data?.hash}`}
+                        >
+                            Etherscan Link
                         </a>
                     </div>
                 </div>
